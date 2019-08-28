6809d54a0f9557b3db8eeaa3b1e3ba16a202d874
James Halgren <james.halgren@noaa.gov>
2019-08-28 14:23:35 -0500

    SteadyNetwork working with reasonable output (viewed with Notebook)

--- trunk/NDHMS/dynamic_channel_routing/SteadyNetwork.py
+++ trunk/NDHMS/dynamic_channel_routing/SteadyNetwork.py
@@ -18,12 +18,12 @@ MANNING_UK = 1.49
 
 def Bernoulli_Energy(WSE, V, hl = 0, gravity = GRAVITY):
     ''' compute flow using Manning's equation '''
-    return (WSE) + V ** 2.0 / (2 * gravity) - hl
+    return (WSE) + (V ** 2.0) / (2 * gravity) - hl
 
-def y_direct(B, n, S0, Q):
+def y_direct(B, n, S0, Q, k=MANNING_SI):
     ''' function to compute error in depth calculation for a guessed depth compared to a calculated depth for a given flow.
         Uses scipy.optimize fmin '''
-    y_optimum = fmin(flow_min, Q/B/3, args=(n, S0, Q, B, MANNING_UK), full_output=True, disp=False)
+    y_optimum = fmin(flow_min, Q/B/3, args=(n, S0, Q, B, k), full_output=True, disp=False)
     return float(y_optimum[0])
 
 def flow_min(y, n, S0, Q, B, k = MANNING_SI):
@@ -31,8 +31,9 @@ def flow_min(y, n, S0, Q, B, k = MANNING_SI):
     epsilon = np.abs(Manning_Q(y, n, S0, B, k) - Q)
     return epsilon
 
-def Manning_Slope(A, n, Q, Rw, k = MANNING_SI):
-    return (n * Q / (k * A * Rw **2/3))
+def Manning_Slope(n, Q, A, Rw, k = MANNING_SI):
+    #print(f'n * Q / (k * A * (Rw ** (2/3)))) ** 2.0 {n} * {Q} / ({k} * {A} * ({Rw} ** (2/3)))) ** 2.0')
+    return (n * Q / (k * A * (Rw ** (2.0/3.0)))) ** 2.0
 
 def Manning_Q(y, n, S0, B, k = MANNING_SI):
     ''' compute flow using Manning's equation '''
@@ -137,26 +138,26 @@ class SteadyNetwork(Network):
         ''' This input option is intended to be an extremely simple channel for testing and plotting development'''
         input_vars = {}
 
-        self.time_list = range(101)
-#        import pandas as pd
-#        pandas.date_range("11:00", "21:30", freq="30min")
-#        datelist = pd.date_range(pd.datetime.today(), periods=100).tolist()
+        self.time_list = range(22)
+        # import pandas as pd
+        # pandas.date_range("11:00", "21:30", freq="30min")
+        # datelist = pd.date_range(pd.datetime.today(), periods=100).tolist()
 
-        n_sections = 40
+        n_sections = 11
         I_UPSTREAM = n_sections - 1
         I_DOWNSTREAM = 0
 
-        station_downstream = 10000
-        station_upstream = 10000000
+        station_downstream = 0
+        station_upstream = 1000000
         stations = np.linspace(station_downstream, station_upstream, n_sections, False)
-        bottom_widths = np.linspace(100, 1000, len(stations), False)
+        bottom_widths = np.linspace(1000, 100, len(stations), False)
         bottom_zs = np.linspace(0,100, len(stations), False)
 
         # print(NCOMP, len(stations))
 
         input_vars.update({"dxini": 1000})
         input_vars.update({"manning_n_ds": 0.035})
-        input_vars.update({"loss_coeff": 0.3})
+        input_vars.update({"loss_coeff": 0.1})
 
         for i, bw in enumerate(bottom_widths):
             # continue
@@ -176,8 +177,8 @@ class SteadyNetwork(Network):
                                             / self.sections[i].dx_ds
 
         #TODO: clean up this code to generate intial upstream flow and downstream stage boundary time series
-        self.upstream_flow_ts = Generate_Hydrograph(len(self.time_list) , 20 , 2 , 4 , 5000)
-        self.downstream_stage_ts = [y_direct(self.sections[I_DOWNSTREAM].bottom_width
+        self.upstream_flow_ts = Generate_Hydrograph(len(self.time_list) , 0 , 7 , 4 , 5000)
+        self.downstream_stage_ts = [5*y_direct(self.sections[I_DOWNSTREAM].bottom_width
                                              , self.sections[I_DOWNSTREAM].manning_n_ds
                                              , self.sections[I_DOWNSTREAM].bed_slope_ds
                                              , q ) for q in self.upstream_flow_ts]
@@ -240,45 +241,111 @@ class SteadyNetwork(Network):
 
             section.time_steps.append(self.TimeStep(new_flow = upstream_flow_next, new_depth = y_standard))
 
-    def y_standard_step(self, j, section_ds, section_us, Q, y_guess = 1.0, k=1.0, gravity=GRAVITY):
+    def y_standard_step(self, j, section_ds, section_us, Q, y_guess = 1.0, k=MANNING_SI, gravity=GRAVITY):
     #TODO: Make GRAVITY and MANNING_K constants consistent with anticipated units and
     #get them to be called/passsed consistently.
         ''' function to compute error in depth calculation for a guessed depth compared to a calculated depth for a given flow.
             Uses scipy.optimize fmin '''
-        y_us = y_guess
         y_ds = section_ds.time_steps[j].depth
-        # print(f'{y_us} {y_ds}')
         z_us = section_us.bottom_z
         z_ds = section_ds.bottom_z
         n_us = section_us.manning_n_ds
         n_ds = section_ds.manning_n_ds
 
-        loss_coeff = section_us.loss_coeff_ds
+        loss_coeff = section_us.loss_coeff_ds # The loss coefficient is a reach property so we use only one of them from the upstream reach
+        dx = section_us.dx_ds # The downstream distance is a reach property so we use only one of them from the upstream reach
+
+        # y_standard = fmin(self.stage_standard_min, y_guess, args=(y_ds, z_us, z_ds, V_us, V_ds, hl_us2ds, gravity), full_output=True, disp=True)
+        y_standard = fmin(self.stage_standard_min, y_guess, args=(section_us, section_ds, loss_coeff, dx, Q, y_ds, z_us, z_ds, n_us, n_ds, gravity, k), full_output=True, disp=False)
+
+        y_us = float(y_standard[0])
+
+        print(f'{dx}')
+        print(f'y_us: {y_us}   y_ds: {y_ds}')
+        print(f'z_us: {z_us}   z_ds: {z_ds}')
+        print(f'n_us: {n_us}   n_ds: {n_ds}')
+        print(f'{loss_coeff}')
 
         Area_us = section_us.get_area_depth(y_us)
         Area_ds = section_ds.get_area_depth(y_ds)
+        print(f'Area_us: {Area_us}   Area_ds: {Area_ds}')
         V_us = Q / Area_us
         V_ds = Q / Area_ds
+        print(f'V_us: {V_us}   V_ds: {V_ds}')
+        V_head_us = V_us ** 2.0 / (2 * gravity) 
+        V_head_ds = V_ds ** 2.0 / (2 * gravity)
+        print(f'V_head_us: {V_head_us}   V_head_ds: {V_head_ds}')
         Pw_us = section_us.get_wetted_perimeter_depth(y_us)
         Pw_ds = section_ds.get_wetted_perimeter_depth(y_ds)
+        print(f'Pw_us: {Pw_us}   Pw_ds: {Pw_ds}')
         Rw_us = Area_us / Pw_us
         Rw_ds = Area_ds / Pw_ds
+        print(f'Rw_us: {Rw_us}   Rw_ds: {Rw_ds}')
         Area_mean = (Area_ds + Area_us) / 2.0
         n_mean = (n_ds + n_us) / 2.0
         Rw_mean = (Rw_ds + Rw_us) / 2.0
-        Sf = Manning_Slope(Q, Area_mean, Rw_mean, n_mean, k)
-        hl_us2ds = Sf * section_us.dx_ds + loss_coeff * abs(V_us ** 2.0 / (2 * gravity) -  V_ds ** 2.0 / (2 * gravity))
-        y_standard = fmin(self.stage_standard_min, y_guess, args=(y_ds, z_us, z_ds, Q, V_us, V_ds, hl_us2ds, MANNING_UK), full_output=True, disp=False)
+        Sf_ds = Manning_Slope(Q = Q, A = Area_ds, Rw = Rw_ds, n = n_ds, k = k)
+        Sf_us = Manning_Slope(Q = Q, A = Area_us, Rw = Rw_us, n = n_us, k = k)
+        Sf_mean = Manning_Slope(Q = Q, A = Area_mean, Rw = Rw_mean, n = n_mean, k = k)
+        #Sf_mean = (Sf_ds + Sf_us) / 2.0
+        print (f'Q {Q}')
+        print(f'Sf_ds = {Sf_ds} Sf_us = {Sf_us} Sf_mean = {Sf_mean}')
+        hl_us2ds = Sf_mean * dx + loss_coeff * abs(V_head_us -  V_head_ds)
+        print('hl_us2ds = {hl_us2ds}')
+        print('y_us, y_guess, y_ds, z_us, z_ds, Q, V_us, V_ds, hl_us2ds, MANNING_SI')
+        print(y_us, y_guess, y_ds, z_us, z_ds, Q, V_us, V_ds, hl_us2ds, MANNING_SI)
+        WSE_ds = y_ds + z_ds
+        WSE_us = y_us + z_us
+        E_ds = Bernoulli_Energy(WSE_ds, V_ds, 0, gravity) 
+        E_us = Bernoulli_Energy(WSE_us, V_us, hl_us2ds, gravity)
+        print(f'E_ds: {E_ds}')
+        print(f'E_us: {E_us}')
+
         return float(y_standard[0])
 
-    def stage_standard_min(self, y_us, y_ds, z_us, z_ds, Q, V_us, V_ds, hl_us2ds = 0, k = MANNING_SI, gravity = GRAVITY):
+    def stage_standard_min(self, y_us, section_us, section_ds, loss_coeff, dx, Q, y_ds, z_us, z_ds, n_us, n_ds, gravity = GRAVITY, k=MANNING_SI):
         ''' computes the error term comparing the Manning's computed depth with the given Q '''
+        Area_us = section_us.get_area_depth(y_us)
+        Area_ds = section_ds.get_area_depth(y_ds)
+        # print(f'{Area_us} {Area_ds}')
+        V_us = Q / Area_us
+        V_ds = Q / Area_ds
+        # print(f'{V_us} {V_ds}')
+        V_head_us = V_us ** 2.0 / (2 * gravity) 
+        V_head_ds = V_ds ** 2.0 / (2 * gravity)
+        # print(f'{V_head_us} {V_head_ds}')
+        Pw_us = section_us.get_wetted_perimeter_depth(y_us)
+        Pw_ds = section_ds.get_wetted_perimeter_depth(y_ds)
+        # print(f'{Pw_us} {Pw_ds}')
+        Rw_us = Area_us / Pw_us
+        Rw_ds = Area_ds / Pw_ds
+        # print(f'{Rw_us} {Rw_ds}')
+
+        # print (f'Q {Q}')
+
+        Area_mean = (Area_ds + Area_us) / 2.0
+        n_mean = (n_ds + n_us) / 2.0
+        Rw_mean = (Rw_ds + Rw_us) / 2.0
+        Sf_ds = Manning_Slope(Q = Q, A = Area_ds, Rw = Rw_ds, n = n_ds, k = k)
+        Sf_us = Manning_Slope(Q = Q, A = Area_us, Rw = Rw_us, n = n_us, k = k)
+        #Sf_mean = Manning_Slope(Q = Q, A = Area_mean, Rw = Rw_mean, n = n_mean, k = k)
+        Sf_mean = (Sf_ds + Sf_us) / 2.0
+        # print(f'Sf_ds = {Sf_ds} Sf_us = {Sf_us} Sf_mean = {Sf_mean}')
+
+        hl_us2ds = Sf_mean * dx + loss_coeff * abs(V_head_us -  V_head_ds)
+        # print('hl_us2ds')
+        # print(hl_us2ds)
+
         # print(y_ds, z_ds)
         # print(y_us, z_us)
         WSE_ds = y_ds + z_ds
         WSE_us = y_us + z_us
-
-        epsilon = np.abs(Bernoulli_Energy(WSE_ds, V_ds) - Bernoulli_Energy(WSE_us, V_us, hl_us2ds))
+        E_ds = Bernoulli_Energy(WSE_ds, V_ds, 0, gravity) 
+        E_us = Bernoulli_Energy(WSE_us, V_us, hl_us2ds, gravity)
+        # print(f'E_ds: {E_ds}')
+        # print(f'E_us: {E_us}')
+        epsilon = np.abs(E_us - E_ds)
+        #epsilon = np.abs(Bernoulli_Energy(WSE_ds, V_ds, 0, gravity) - Bernoulli_Energy(WSE_us, V_us, hl_us2ds, gravity))
         return epsilon
 
 
@@ -300,7 +367,7 @@ class Section:
         self.bottom_z = bottom_z
         self.manning_n_ds = manning_n_ds
         self.time_steps = [] # array of values
-        self.sk = MANNING_UK
+        self.sk = MANNING_SI
 
         #Time independent downstream reach properties
         self.dx_ds = 0 # Distance to downstream section
@@ -318,10 +385,10 @@ class Section:
         return self.bottom_z * self.time_steps[j].depth
 
     def get_wetted_perimeter_depth(self, depth):
-        return self.bottom_width * depth
+        return self.bottom_width + 2.0 * depth
 
     def get_wetted_perimeter_j(self, j):
-        return self.bottom_z * self.time_steps[j].depth
+        return self.bottom_z + 2.0 * self.time_steps[j].depth
 
 def main():
     # network = DummyNetwork()
@@ -331,22 +398,8 @@ def main():
     # network = MESHDNetwork()
 
     network.input_and_initialize()
-
     network.compute_initial_state()
-    network.compute_time_steps() #TODO: ask Nick -- should this even require an argument?
-
-    cols_for_subplots = 4
-    fig, axes = plt.subplots(nrows=ceil(len(network.sections)/cols_for_subplots), ncols=cols_for_subplots, squeeze=False)
-
-    #TODO: make plotting another method within the network
-    for i, section in enumerate(network.sections):
-        a = pd.Series(time_step.depth for i, time_step in enumerate(section.time_steps))
-        # print(m, n)
-        m = i//cols_for_subplots
-        n = i%cols_for_subplots
-        a.plot(ax = axes[m,n])
-
-    return
+    network.compute_time_steps()
 
 if __name__ == "__main__":
     main()
