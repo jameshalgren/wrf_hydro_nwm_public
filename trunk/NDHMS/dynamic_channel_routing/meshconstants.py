4f04233704cf3633661342c1407e16ef5e9ea56d
James Halgren <james.halgren@noaa.gov>
2019-09-11 16:40:41 -0500

    added meshconstants.py

---
+++ trunk/NDHMS/dynamic_channel_routing/meshconstants.py
@@ -0,0 +1,14 @@
+PHI = 1.0
+THETA = 1.0
+THETAS = 1.0
+TESINV = 1.0
+ALFA2 = 0.5
+ALFA4 = 0.1
+'''
+1.0 =: phi           source term treatment (0:explicit, 1:implicit)
+1.0 =: theta         ?
+1.0 =: thetas        ?
+1.0 =: thesinv       ?
+0.5 =: alfa2         emp parameter for artificial diffusion (lit)
+0.1 =: alfa4         maximum value for artificial diffusion
+'''
