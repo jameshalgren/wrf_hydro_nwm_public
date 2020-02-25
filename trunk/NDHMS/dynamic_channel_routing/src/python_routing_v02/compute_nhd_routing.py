40337ec33fb02ea71af3e49f87d08e822f365594
James Halgren <james.halgren@noaa.gov>
2020-02-25 12:59:14 -0600

    fill stub for ordered computation

--- trunk/NDHMS/dynamic_channel_routing/src/python_routing_v02/compute_nhd_routing.py
+++ trunk/NDHMS/dynamic_channel_routing/src/python_routing_v02/compute_nhd_routing.py
@@ -27,9 +27,9 @@ def set_network():
     geo_input_folder = os.path.join(test_folder, r'input', r'geo', r'Channels')
 
     """##NHD Subset (Brazos/Lower Colorado)"""
-    Brazos_LowerColorado_ge5 = True
+    Brazos_LowerColorado_ge5 = False
     """##NHD CONUS order 5 and greater"""
-    CONUS_ge5 = False
+    CONUS_ge5 = True
     """These are large -- be careful"""
     CONUS_FULL_RES_v20 = False
     CONUS_Named_Streams = False #create a subset of the full resolution by reading the GNIS field
@@ -87,30 +87,30 @@ def set_network():
                 print('\n\nWARNING: If this works, you may be using old data...')
 
             
-            # Use only headwater keys that are in the full dataset.
-            headwater_keys_combined = CONUS_FULL_RES_v20_values[3] & \
+            # Use only headwater segments that are in the full dataset.
+            headwater_segments_combined = CONUS_FULL_RES_v20_values[3] & \
                                         CONUS_Named_Streams_values[3]
             # Need to make sure that these objects are independent -- we will modify them a bit.
             connections_combined = pickle.loads(pickle.dumps(CONUS_FULL_RES_v20_values[0]))
-            terminal_keys_combined = pickle.loads(pickle.dumps(CONUS_FULL_RES_v20_values[4]))
+            terminal_segments_combined = pickle.loads(pickle.dumps(CONUS_FULL_RES_v20_values[4]))
             terminal_code_combined = CONUS_Named_Streams_supernetwork['terminal_code']
             
-            for key in connections_combined: #Clear the upstreams and rebuild it with just named streams
-                connections_combined[key].pop('upstreams',None)
+            for segment in connections_combined: #Clear the upstreams and rebuild it with just named streams
+                connections_combined[segment].pop('upstreams',None)
 
-            (junction_keys_combined
-            , visited_keys_combined
-            , visited_terminal_keys_combined
+            (junction_segments_combined
+            , visited_segments_combined
+            , visited_terminal_segments_combined
             , junction_count_combined) = networkbuilder.get_up_connections(
                         connections = connections_combined
                         , terminal_code = terminal_code_combined
-                        , headwater_keys = headwater_keys_combined
-                        , terminal_keys = terminal_keys_combined
+                        , headwater_segments = headwater_segments_combined
+                        , terminal_segments = terminal_segments_combined
                         , verbose = verbose
                         , debuglevel = debuglevel)
 
 def recursive_junction_read (
-                             keys
+                             segments
                              , order_iter
                              , con
                              , network
@@ -119,62 +119,70 @@ def recursive_junction_read (
                              , debuglevel = 0
                             ):
     #global con, network
-    for key in keys:
-        ckey = key
+
+    #Greatest to least ordering of the river system for computation.
+    #IDs are in order of magnitude so largest order IDs will be computed first from the list reading left to right
+    #Can easily change to great sublists for each magnitude of order if necessary
+    #temporary function to gather unique order listings
+
+    for segment in segments:
+        csegment = segment
         if 1 == 1:
         #try:
             reach = {}
-            reach.update({'reach_tail':ckey})
+            reach.update({'reach_tail':csegment})
             reachset = set()
-            reachset.add(ckey)
-            ukeys = con[key]['upstreams']
-            while not len(ukeys) >= 2 and not (ukeys == {terminal_code}):
-                if debuglevel <= -3: print(f"segs at ckey {ckey}: {network['total_segment_count']}")
+            reachset.add(csegment)
+            usegments = con[segment]['upstreams']
+            while True: 
+                if usegments == {terminal_code}: # HEADWATERS
+                    if debuglevel <= -3: print(f"headwater found at {csegment}")
+                    network['total_segment_count'] += 1
+                    if debuglevel <= -3: print(f"segs at csegment {csegment}: {network['total_segment_count']}")
+                    reachset.add(csegment)
+                    reach.update({'reach_head':csegment})
+                    reach.update({'order':order_iter})
+                    network.update({'maximum_order':max(network['maximum_order'],order_iter)})
+                    reach.update({'segments':reachset})
+                    network['reaches'].update({csegment:reach})
+                    network['headwaters'].add(csegment)
+                    break
+                elif len(usegments) >= 2: # JUNCTIONS
+                    if debuglevel <= -3: print(f"junction found at {csegment} with upstreams {usegments}")
+                    network['total_segment_count'] += 1
+                    if debuglevel <= -3: print(f"segs at csegment {csegment}: {network['total_segment_count']}")
+                    reachset.add(csegment)
+                    reach.update({'reach_head':csegment})
+                    reach.update({'order':order_iter})
+                    network.update({'maximum_order':max(network['maximum_order'],order_iter)})
+                    reach.update({'segments':reachset})
+                    network['reaches'].update({csegment:reach})
+                    network['total_junction_count'] += 1 #the Terminal Segment
+                    network['junctions'].add(csegment)
+                    recursive_junction_read (
+                            usegments
+                            , order_iter + 1
+                            , con
+                            , network
+                            , terminal_code = terminal_code
+                            , verbose = verbose
+                            , debuglevel = debuglevel) 
+                    break
+                if debuglevel <= -3: print(f"segs at csegment {csegment}: {network['total_segment_count']}")
                 # the terminal code will indicate a headwater
-                if debuglevel <= -4: print(ukeys)
-                (ckey,) = ukeys
-                ukeys = con[ckey]['upstreams']
+                if debuglevel <= -4: print(usegments)
+                (csegment,) = usegments
+                usegments = con[csegment]['upstreams']
                 network['total_segment_count'] += 1
-                reachset.add(ckey)
-                #TODO: Can this be indented?
-            if ukeys == {terminal_code}: # HEADWATERS
-                if debuglevel <= -3: print(f"headwater found at {ckey}")
-                network['total_segment_count'] += 1
-                if debuglevel <= -3: print(f"segs at ckey {ckey}: {network['total_segment_count']}")
-                reachset.add(ckey)
-                reach.update({'reach_head':ckey})
-                reach.update({'order':order_iter})
-                network.update({'maximum_order':max(network['maximum_order'],order_iter)})
-                reach.update({'segments':reachset})
-                network['reaches'].update({ckey:reach})
-                network['headwaters'].add(ckey)
-            elif len(ukeys) >= 2: # JUNCTIONS
-                if debuglevel <= -3: print(f"junction found at {ckey} with upstreams {ukeys}")
-                network['total_segment_count'] += 1
-                if debuglevel <= -3: print(f"segs at ckey {ckey}: {network['total_segment_count']}")
-                reachset.add(ckey)
-                reach.update({'reach_head':ckey})
-                reach.update({'order':order_iter})
-                network.update({'maximum_order':max(network['maximum_order'],order_iter)})
-                reach.update({'segments':reachset})
-                network['reaches'].update({ckey:reach})
-                network['total_junction_count'] += 1 #the Terminal Segment
-                network['junctions'].add(ckey)
-                recursive_junction_read (
-                        ukeys
-                        , order_iter + 1
-                        , con
-                        , network
-                        , terminal_code = terminal_code
-                        , verbose = verbose
-                        , debuglevel = debuglevel) 
-                # print(ukeys)
+                reachset.add(csegment)
+
+                # print(usegments)
         #except:
             #if debuglevel <= -2: 
-                #print(f'There is a problem with connection: {key}: {con[key]}')
+                #print(f'There is a problem with connection: {segment}: {con[segment]}')
 
 def network_trace(
-                        nid
+                        terminal_segment
                         , order_iter
                         , con
                         , terminal_code = 0
@@ -185,7 +193,7 @@ def network_trace(
     network = {}
     us_length_total = 0
     
-    if verbose: print(f'\ntraversing upstream on network {nid}:')
+    if verbose: print(f'\ntraversing upstream on network {terminal_segment}:')
     # try:
     if 1 == 1:
         network.update({'total_segment_count': 0}) 
@@ -195,7 +203,7 @@ def network_trace(
         network.update({'headwaters':set()})
         network.update({'reaches':{}}) #the Terminal Segment
         recursive_junction_read(
-                  [nid]
+                  [terminal_segment]
                   , order_iter
                   , con
                   , network
@@ -207,7 +215,7 @@ def network_trace(
     # except Exception as exc:
     #     print(exc)
     #TODO: compute upstream length as a surrogate for the routing computation
-    return {nid: network, 'upstream_length': us_length_total}
+    return {terminal_segment: network, 'upstream_length': us_length_total}
 
 def compose_reaches(
         supernetwork_values = None
@@ -216,13 +224,13 @@ def compose_reaches(
         , verbose = False
         ):
 
-    terminal_keys = supernetwork_values[4] 
-    circular_keys = supernetwork_values[6]
-    terminal_keys_super = terminal_keys - circular_keys
+    terminal_segments = supernetwork_values[4] 
+    circular_segments = supernetwork_values[6]
+    terminal_segments_super = terminal_segments - circular_segments
     con = supernetwork_values[0]
         
-    networks = {terminal_key:{}
-                      for terminal_key in terminal_keys_super 
+    networks = {terminal_segment:{}
+                      for terminal_segment in terminal_segments_super 
                      }  
     debuglevel = -2
     verbose = False
@@ -235,70 +243,104 @@ def compose_reaches(
     start_time = time.time()
     results_serial = {}
     init_order = 0
-    for nid, network in networks.items():
-        network.update(network_trace(nid, init_order, con, terminal_code = terminal_code, verbose = verbose, debuglevel = debuglevel)[nid])
+    for terminal_segment, network in networks.items():
+        network.update(network_trace(terminal_segment, init_order, con, terminal_code = terminal_code, verbose = verbose, debuglevel = debuglevel)[terminal_segment])
     print("--- %s seconds: serial compute ---" % (time.time() - start_time))
     if debuglevel <= -1: print(f'Number of networks in the Supernetwork: {len(networks.items())}')
 
     if debuglevel <= -2:
-        for nid, network in networks.items():
-            print(f'terminal_key: {nid}')
+        for terminal_segment, network in networks.items():
+            print(f'terminal_segment: {terminal_segment}')
             for k, v in network.items():
                 if type(v) is dict:
                     print (f'{k}:')
                     for k1, v1 in v.items():
-                        print(f'{k1}: {v1}')
-
+                        print(f'\t{k1}: {v1}')
                 else: print(f'{k}: {v}')
-
     return networks
 
-def compute_reach(reach = None):
+def compute_network(
+        terminal_segment = None
+        , network = None
+        , con = None
+        , verbose = False
+        , debuglevel = 0
+        ):
+    if verbose: print(f"\n\nExecuting simulation on network {terminal_segment} beginning with streams of order {network['maximum_order']}")
+
+    for x in range(network['maximum_order'],-1,-1):
+        for junction_segment, reach in network['reaches'].items():
+            if x == reach['order']:
+                compute_reach(junction_segment, reach, con, verbose, debuglevel)
+
+def compute_reach(
+        junction_segment = None
+        , reach = None
+        , con = None
+        , verbose = False
+        , debuglevel = 0
+        ):
+    if verbose: print(f"\nreach: {junction_segment} (order: {reach['order']} n_segs: {len(reach['segments'])})")
+    current_segment = reach['reach_head']
+    next_segment = con[current_segment]['downstream'] 
+    while True:
+        # Thanks to SO post for a reminder of this "Loop-and-a-half" construct
+        # https://stackoverflow.com/questions/1662161/is-there-a-do-until-in-python
+        if current_segment == reach['reach_tail']:
+            if verbose: print(f'{current_segment} (tail)')
+            break
+        if verbose: print(f'{current_segment} --> {next_segment}')
+        current_segment = next_segment
+        next_segment = con[current_segment]['downstream'] 
+
+
+#        print(g2l)
+#        reordered = []
+#        for x in g2l:
+#            for y,z in con.items():
+#                if x == y:
+#                    reordered.append({y:z})
+#
+#
+#        print(reordered)
+
+def compute_segment():
     pass
 
-def compute_segment:
+def get_upstream_inflow():
+    pass
+
+def get_lateral_inflow():
+    pass
+
+def compute_junction_downstream():
     pass
 
 def main():
+
+    debuglevel = -1
+    verbose = True
+
+
+    if verbose: print('creating supernetwork connections set')
+    start_time = time.time()
     supernetwork_values = set_network()
+    if verbose: print('supernetwork connections set complete')
+    if debuglevel <= -1: print("--- in %s seconds: ---" % (time.time() - start_time))
+    if verbose: print('ordering reaches ...')
+    start_time = time.time()
     networks = compose_reaches(supernetwork_values)
+    if verbose: print('ordered reaches complete')
+    if debuglevel <= -1: print("--- in %s seconds: ---" % (time.time() - start_time))
 
-    #Greatest to least ordering of the river system for computation.
-    #IDs are in order of magnitude so largest order IDs will be computed first from the list reading left to right
-    #Can easily change to great sublists for each magnitude of order if necessary
-    #temporary function to gather unique order listings
-
+    start_time = time.time()
+    if verbose: print('executing computation on ordered reaches ...')
     con = supernetwork_values[0]
-    for terminal_key, network in networks.items():
+    for terminal_segment, network in networks.items():
+        compute_network(terminal_segment, network, con, verbose, debuglevel)
+    if verbose: print('ordered reach computation complete')
+    if debuglevel <= -1: print("--- in %s seconds: ---" % (time.time() - start_time))
 
-        g2l = []
-        print([x for x in range(network['maximum_order'],-1,-1)])
-        for x in range(network['maximum_order'],-1,-1):
-            for y, z in network['reaches'].items():
-                if x == z['order']:
-                    g2l.append(y)
-
-        print(g2l)
-        reordered = []
-        for x in g2l:
-            for y,z in con.items():
-                if x == y:
-                    reordered.append({y:z})
-
-        print(reordered)
 
 if __name__ == '__main__':
     main()
-
-    # # nhd_conus_networks = []
-    # # conus_supernetwork = SuperNetwork()
-    #
-    # print([network.networkID for network in nhd_conus_networks])
-    # print([network.reachCollection for network in nhd_conus_networks])
-    # print((network.reachCollection for network in nhd_conus_networks))
-    # print(network.reachCollection for network in nhd_conus_networks)
-    # for network in nhd_conus_networks:
-    #     print([reach.reachID for reach in network.reachCollection])
-
-    # print([reach.order for reach in network.reachCollection
-                                    # for network in nhd_conus_networks])
